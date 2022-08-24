89e24c1f36a63ba013c758b8f3d76c597f7a4d57
Martin Vidal Freyre <martinvidal.n7@gmail.com>
2022-08-24 16:43:41 -0300

    FC

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+
+
+
+// Mensaje de Bienvenida
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
+
+
+
+
+
+// Mensaje de Despedida
+console.log("Adios")
